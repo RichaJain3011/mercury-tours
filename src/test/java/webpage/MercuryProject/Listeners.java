37ddbd3fe2b5b3a6256f1f880f9bb4d819e5069e
richajain3011 <richajain3011@gmail.com>
2018-06-13 15:46:37 -0400

    commit2

---
+++ src/test/java/webpage/MercuryProject/Listeners.java
@@ -0,0 +1,45 @@
+package webpage.MercuryProject;
+
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+import org.testng.internal.annotations.IListeners;
+
+public class Listeners implements ITestListener {
+
+	public void onTestStart(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onTestSuccess(ITestResult result) {
+		System.out.println(result.getName());
+		
+	}
+
+	public void onTestFailure(ITestResult result) {
+		System.out.println(result.getName());
+		
+	}
+
+	public void onTestSkipped(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onStart(ITestContext context) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onFinish(ITestContext context) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
